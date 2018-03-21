50aabc849fca4fb1aebe33e222b6afdb136495f3
Matt Lebrun <matt@lebrun.org>
2018-03-21 17:42:58 +0800

    Added more scripts

---
+++ sys/bin/get-docker-images.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+docker pull ubuntu:16.04
+docker pull postgres:latest
+docker pull redis:latest
+docker pull rabbitmq:management
+
+docker pull mongodb:latest
+docker pull couchdb:latest
+docker pull nginx:latest
+docker pull memcached:latest
+docker pull python:3.6
---
+++ sys/bin/svc_ipega_controller.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# Need to run this as root
+
+# 1. Install the xboxdrv package
+#
+#    $ sudo apt install xboxdrv
+#
+# 2. Connect your Ipega controller to the PC via BT
+#
+#    HINT:
+#    press X + Home on the controller to activate Gamepad mode and broadcast
+#    the BT signal
+#
+# 3. Get the event number of the contoller via: 
+#
+#    $ cat /proc/bus/input/devices
+#
+# 4. Update the IPEGA_EVENT variable here with the correct eventXX ID.
+#
+IPEGA_EVENT=event14
+
+sudo xboxdrv \
+    --evdev /dev/input/$IPEGA_EVENT \
+    --evdev-absmap \
+        ABS_X=x1,ABS_Y=y1,ABS_Z=x2,ABS_RZ=y2,\
+        ABS_HAT0X=dpad_x,ABS_HAT0Y=dpad_y \
+    --axismap -Y1=Y1,-Y2=Y2 \
+    --evdev-keymap \
+        BTN_A=a,BTN_B=b,BTN_X=x,BTN_Y=y,\
+        BTN_TL=lb,BTN_TR=rb,BTN_TL2=lt,BTN_TR2=rt,\
+        BTN_THUMBL=tl,BTN_THUMBR=tr,\
+        BTN_SELECT=back,BTN_START=start \
+    --mimic-xpad-wireless \
+    --silent
